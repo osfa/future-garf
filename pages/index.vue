2a99c3accae2928a01963b30301520ce2cdde1cf
osfa <jacob.broms@prototyp.se>
2022-03-22 19:08:08 +0100

    debug 404

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,15 @@
 <template>
   <!-- <div :style="{ backgroundImage: `url(${image})`}"> -->
+    <div>
+            <Tutorial />
+
   <nuxt-img
     src="/imgs/1987-12-11-0-10e-1646988554-1024x2048-10e-1646990976-splinter-0-gigapixel-lines-scale-2_00x.png"
     alt="Space by Jeremy Thomas on Unsplash"
-    sizes="xl:100vw lg:100vw md:100vw sm:100vw xs:100vw"
+    sizes="xl:50vw lg:50vw md:50vw sm:100vw xs:100vw"
     />
-  <!-- </div> -->
- <!--  <Tutorial /> -->
+  </div>
+
 </template>
 
 <script>
