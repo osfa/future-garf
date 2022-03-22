b4bf618c268795e3e3a84fdc48211ed22b119c01
osfa <jacob.broms@prototyp.se>
2022-03-23 00:01:07 +0100

    testtyy

--- pages/index.vue
+++ pages/index.vue
@@ -24,55 +24,62 @@
 // import bkgImg from "/imgs/1987-12-11-0-10e-1646988554-1024x2048-10e-1646990976-splinter-0-gigapixel-lines-scale-2_00x.png";
 export default {
   name: 'IndexPage',
+  // props: {
+  //   containerWidth: { type: Number, required: true }
+  // },
   data() {
     return {
+      mainImageUrl: '/imgs/2048-false-floyd.png',
+      requestUrl: '',
       image: `${require('@/static/imgs/cmyk-false-floyd-test.png?w=1280&nf_resize=fit')}`,    
     };
   },
   computed: {
     backgroundStyles() {
-      let imgUrl = this.$img('/imgs/cmyk-false-floyd-test.png', { width: 960 })
+      let baseUrl = '/imgs/cmyk-false-floyd.png'
+      baseUrl = this.mainImageUrl
+      let imgUrl = this.$img(baseUrl, { width: 960 })
       if (process.client) {
         const width = window.innerWidth;
         console.log(width)
         if (width > 2000){
-          imgUrl = this.$img('/imgs/cmyk-false-floyd-test.png', { width: 2600 })
+          imgUrl = this.$img(baseUrl, { width: 2600 })
         }
         else if (width > 1534){
-          imgUrl = this.$img('/imgs/cmyk-false-floyd-test.png', { width: 1920 })
+          imgUrl = this.$img(baseUrl, { width: 1920 })
         }
         else if(width > 1280){
-          imgUrl = this.$img('/imgs/cmyk-false-floyd-test.png', { width: 1534 })
+          imgUrl = this.$img(baseUrl, { width: 1534 })
         }
         else if(width > 1024){
-          imgUrl = this.$img('/imgs/cmyk-false-floyd-test-v.png', { width: 1280 })
+          imgUrl = this.$img(baseUrl, { width: 1280 })
         }
         else {
-          imgUrl = this.$img('/imgs/cmyk-false-floyd-test-v.png', { width: 1280 })
+          imgUrl = this.$img(baseUrl, { width: 1280 })
         }
       }
       return {
         backgroundImage: `url('${imgUrl}')`
       }
     }
-  }
+  },
+  beforeMount() {
+    window.addEventListener("resize", this.handleResize);
+  },
+  beforeDestroy() {
+    window.removeEventListener("resize", this.handleResize);
+  },
+  methods: {
+    handleResize(e) {
+      console.log('resize and force update?')
+      this.$forceUpdate();
+      // const baseUrl = '/imgs/2048-false-floyd.png'
+      // let imgUrl = this.$img(baseUrl, { width: 960 })
+      // this.containerWidth = window.innerWidth
+    }
+  },
 }
 </script>
-<!-- computed: {
-    css () {
-      if (process.client) {
-        let width = window.innerWidth
-
-        // ... mobile { ... styles }
-        // ... desktop { ... styles }
-
-        // ... if width is less than 700, return mobile
-        // ... if width greater than 700, return desktop
-      } else {
-        return { /*empty style object*/ }
-      }
-    }
-  } -->
 <style scoped>
 .page {
   width:  100vw;
