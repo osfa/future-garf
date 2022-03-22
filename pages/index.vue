9f0d3d523fde55385eae917513d060be272bda9f
osfa <jacob.broms@prototyp.se>
2022-03-22 19:02:28 +0100

    hmm

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,22 @@
 <template>
-      <nuxt-img
+  <!-- <div :style="{ backgroundImage: `url(${image})`}"> -->
+  <nuxt-img
     src="/imgs/1987-12-11-0-10e-1646988554-1024x2048-10e-1646990976-splinter-0-gigapixel-lines-scale-2_00x.png"
     alt="Space by Jeremy Thomas on Unsplash"
     sizes="xl:100vw lg:100vw md:100vw sm:100vw xs:100vw"
     />
+  <!-- </div> -->
  <!--  <Tutorial /> -->
 </template>
 
 <script>
+// import bkgImg from "/imgs/1987-12-11-0-10e-1646988554-1024x2048-10e-1646990976-splinter-0-gigapixel-lines-scale-2_00x.png";
 export default {
   name: 'IndexPage',
+  data() {
+    return {
+      image: `url(${require('@/static/imgs/1987-12-11-0-10e-1646988554-1024x2048-10e-1646990976-splinter-0-gigapixel-lines-scale-2_00x.png')})`,    
+    };
+  }
 }
 </script>
