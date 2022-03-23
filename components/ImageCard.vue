5d014f994740515e7298de8e5bb540ce38ddc919
osfa <jacob.broms@prototyp.se>
2022-03-23 22:40:57 +0100

    crude anims

---
+++ components/ImageCard.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class='page' :style='backgroundStyles'>
+    </div>
+</template>
+
+<script>
+// import bkgImg from '/imgs/1987-12-11-0-10e-1646988554-1024x2048-10e-1646990976-splinter-0-gigapixel-lines-scale-2_00x.png';
+export default {
+  name: 'ImageCard',
+  props: {
+    mainImageUrl: { type: String, required: true },
+    currentWidth: { type: Number, required: true }
+  },
+  // data() {
+  //   return {
+  //     currentWidth: '',
+  //   }
+  // },
+  computed: {
+    backgroundStyles() {
+      let imgUrl = this.mainImageUrl
+      if (process.client) {
+        if (this.currentWidth < 1024){
+          imgUrl = imgUrl.replace('.png', '-v.png')
+        }
+      }
+      return {
+        backgroundImage: `url('${imgUrl}')`
+      }
+    },
+  },
+  // beforeMount() {
+  //   this.currentWidth = window.innerWidth
+  //   window.addEventListener('resize', this.handleResize);
+  // },
+  // beforeDestroy() {
+  //   window.removeEventListener('resize', this.handleResize);
+  // },
+  methods: {
+    // handleResize(e) {
+    //   this.currentWidth = window.innerWidth
+    // },
+    // clickHandler(e) {
+    //   console.log('clickHandler')
+    // }
+  },
+}
+</script>
+<style scoped>
+.page {
+  background-color: #fff;
+  width:  100vw;
+  height:  100vh;
+  background-size: 98%;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,98 +1,50 @@
 <template>
-    <!-- <div class='page' :style='{ backgroundImage: `url(${image})`}'> -->
-    <div class='page' :style='randomBackgroundStyles' @click="newRandomBackground()">
-    <!-- <nuxt-img
-      id='vert'
-      class='lg:hidden w-full h-full'
-      src='/imgs/cmyk-false-floyd-test-v.png'
-      alt='vert'
-      fit='contain'
-      sizes='xl:100vw lg:100vw md:100vw sm:100vw xs:100vw'
-    />
-    <nuxt-img
-      id='hori'
-      fit='contain'
-      class='hidden lg:block w-full h-full'
-      src='/imgs/cmyk-false-floyd-test.png'
-      alt='hori'
-      sizes='xl:100vw lg:100vw md:100vw sm:100vw xs:100vw'
-    /> -->
+    <div>
+        <transition name="fade">
+          <ImageCard v-show='firstCardActive' :main-image-url='mainImageUrl' :current-width='currentWidth' @click.native="toggle()"/>
+        </transition>
+        <transition name="fade">
+          <ImageCard v-show='secondCardActive' :main-image-url='backImageUrl' :current-width='currentWidth' @click.native="toggle()"/>
+        </transition>
     </div>
 </template>
 
+
+<!-- have some sort of list that u pop? -->
 <script>
+import ImageCard from '../components/ImageCard'
+const allImgs = [
+  '/imgs/dithered-8bit/1992-09-22-2-10e.png',
+  '/imgs/dithered-8bit/1992-09-22-2-25e.png',
+  '/imgs/dithered-8bit/1992-09-22-2-5e.png',
+  '/imgs/dithered-8bit/1992-10-01-0-25e.png',
+  '/imgs/dithered-8bit/1992-10-01-0-5e.png',
+  '/imgs/dithered-8bit/1992-10-01-0-60e.png',
+  '/imgs/dithered-8bit/1992-10-01-1-10e.png',
+  '/imgs/dithered-8bit/1992-10-01-1-25e.png',
+  '/imgs/dithered-8bit/1992-10-01-1-5e.png',
+  '/imgs/dithered-8bit/1992-10-01-1-60e.png',
+  '/imgs/dithered-8bit/1992-10-01-2-10e.png',
+  '/imgs/dithered-8bit/1992-10-01-2-25e.png',
+  '/imgs/dithered-8bit/1992-10-01-2-5e.png',
+  '/imgs/dithered-8bit/1992-10-10-0-10e.png',
+  '/imgs/dithered-8bit/1992-10-10-0-25e.png',
+  '/imgs/dithered-8bit/1992-10-10-0-5e.png',
+  '/imgs/dithered-8bit/1992-10-10-0-60e.png',
+]
 // import bkgImg from '/imgs/1987-12-11-0-10e-1646988554-1024x2048-10e-1646990976-splinter-0-gigapixel-lines-scale-2_00x.png';
 export default {
   name: 'IndexPage',
-  // props: {
-  //   containerWidth: { type: Number, required: true }
-  // },
+  components: { ImageCard },
   data() {
     return {
-      imgs: [
-        '/imgs/dithered-8bit/1992-09-22-2-10e.png',
-        '/imgs/dithered-8bit/1992-09-22-2-25e.png',
-        '/imgs/dithered-8bit/1992-09-22-2-5e.png',
-        '/imgs/dithered-8bit/1992-10-01-0-25e.png',
-        '/imgs/dithered-8bit/1992-10-01-0-5e.png',
-        '/imgs/dithered-8bit/1992-10-01-0-60e.png',
-        '/imgs/dithered-8bit/1992-10-01-1-10e.png',
-        '/imgs/dithered-8bit/1992-10-01-1-25e.png',
-        '/imgs/dithered-8bit/1992-10-01-1-5e.png',
-        '/imgs/dithered-8bit/1992-10-01-1-60e.png',
-        '/imgs/dithered-8bit/1992-10-01-2-10e.png',
-        '/imgs/dithered-8bit/1992-10-01-2-25e.png',
-        '/imgs/dithered-8bit/1992-10-01-2-5e.png',
-        '/imgs/dithered-8bit/1992-10-10-0-10e.png',
-        '/imgs/dithered-8bit/1992-10-10-0-25e.png',
-        '/imgs/dithered-8bit/1992-10-10-0-5e.png',
-        '/imgs/dithered-8bit/1992-10-10-0-60e.png',
-      ],
-      mainImageUrl: '/imgs/dithered-8bit/1992-09-22-2-10e.png',
-      currentWidth: '',
-      // image: `${require('@/static/imgs/dithered-8bit/1992-10-10-0-60e.png?w=1280&nf_resize=fit')}`,    
+      imgs: allImgs,
+      mainImageUrl: allImgs[0],
+      backImageUrl: allImgs[1],
+      currentWidth: 1280,
+      firstCardActive: true,
+      secondCardActive: false,
     };
-  },
-  computed: {
-    randomBackgroundStyles() {
-      let imgUrl = this.mainImageUrl
-      if (process.client) {
-        if (this.currentWidth < 1024){
-          imgUrl = imgUrl.replace('.png', '-v.png')
-        }
-      }
-      return {
-        backgroundImage: `url('${imgUrl}')`
-      }
-    },
-    backgroundStyles() {
-      console.log('backgroundStyles?')
-      const baseUrl = this.mainImageUrl
-      let imgUrl = this.$img(baseUrl, { width: 960 })
-      if (process.client) {
-        if (this.currentWidth > 2000){
-          imgUrl = this.$img(baseUrl, { width: 2600 })
-        }
-        else if (this.currentWidth > 1534){
-          imgUrl = this.$img(baseUrl, { width: 1920 })
-        }
-        else if(this.currentWidth > 1280){
-          imgUrl = this.$img(baseUrl, { width: 1534 })
-        }
-        else if(this.currentWidth > 1024){
-          imgUrl = this.$img(baseUrl, { width: 1280 })
-        }
-        // else {
-        //   // baseUrl = '/imgs/1992-10-01-2-60e-1647998796-1024x2048-60e-1648001223-splinter-0-512-perc-32.png'
-        //   // baseUrl = '/imgs/1992-10-01-2-60e-1647998796-1024x2048-60e-1648001223-splinter-0-16-perc.png'
-        //   // baseUrl = '/imgs/1992-10-01-2-60e-1647998796-1024x2048-60e-1648001223-splinter-0-percp8.png'
-        //   imgUrl = this.$img(this.randomItem(), { width: 1280 })
-        // }
-      }
-      return {
-        backgroundImage: `url('${imgUrl}')`
-      }
-    }
   },
   beforeMount() {
     this.currentWidth = window.innerWidth
@@ -102,40 +54,82 @@ export default {
     window.removeEventListener('resize', this.handleResize);
   },
   methods: {
+    randomBackgroundUrl() {
+      return this.imgs[Math.floor(Math.random()*this.imgs.length)]
+    },
     newRandomBackground() {
       const oldUrl = this.mainImageUrl
       while(oldUrl === this.mainImageUrl){
-        this.mainImageUrl = this.imgs[Math.floor(Math.random()*this.imgs.length)]
+        this.mainImageUrl = this.randomBackgroundUrl()
       }
-      // return this.imgs[Math.floor(Math.random()*this.imgs.length)]
+    },
+    toggle(e) {
+      // if(this.active){
+      //   this.mainImageUrl = this.randomBackgroundUrl()
+      // }
+      // else {
+      //   this.backImageUrl = this.randomBackgroundUrl()
+      // }
+      this.firstCardActive = !this.firstCardActive
+      this.secondCardActive = !this.secondCardActive
     },
     handleResize(e) {
-      // console.log('resize and force update?')
-      // console.log('should mutate a prop here? its coming back to me..')
       this.currentWidth = window.innerWidth
-      // this.newRandomBackground()
-      // this.$forceUpdate();
-    }
+    },
   },
 }
 </script>
 <style scoped>
+
 .page {
+  overflow: hidden;
+  position: absolute;
+  left: 0;
+  top: 0;
   background-color: #fff;
-  width:  100vw;
-  height:  100vh;
+  width: 100vw;
+  height: 100vh;
   background-size: 98%;
   background-repeat: no-repeat;
-  background-position: top;
+  background-position: center;
   background-size: cover;
-  /*background-size: clamp(1000px, 80%, 1500px) auto;*/
-/*  max-width: 2048px;
-  background-position: center;*/
-/*  width: 96vw;
-  height: 96vh;
-  margin-top: 2vh;
-  margin-bottom: 2vh;
-  margin-left:  2vw;
-  margin-right: 2vw;*/
+}
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 5s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
+.slide-fade-enter-active {
+  transition: all 0.3s ease-out;
+}
+
+.slide-fade-leave-active {
+  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+
+.slide-fade-enter-from,
+.slide-fade-leave-to {
+  transform: translateX(20px);
+  opacity: 0;
+}
+
+.bounce-enter-active {
+  animation: bounce-in 0.5s;
+}
+.bounce-leave-active {
+  animation: bounce-in 0.5s reverse;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.25);
+  }
+  100% {
+    transform: scale(1);
+  }
 }
 </style>
