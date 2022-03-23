97f0983c8ed5dd6aa7ff302cce5a3cac5d861b5c
osfa <jacob.broms@prototyp.se>
2022-03-24 00:53:55 +0100

    tone etc

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,7 +64,17 @@ export default {
   content: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+    build: {
+    extend(config, ctx) {
+      config.module.rules.push({
+        test: /\.(ogg|mp3|wav|mpe?g)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]'
+        }
+      })
+    }
+  },
 
   image: {
     provider: 'netlify',
