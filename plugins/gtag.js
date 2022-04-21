23856d7008f0aa2f7671849799b395464dc64a0d
osfa <jacob.broms@prototyp.se>
2022-04-21 09:41:17 +0200

    auto...

--- plugins/gtag.js
+++ plugins/gtag.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 
+// https://matteo-gabriele.gitbook.io/vue-gtag/v/master/auto-tracking
+// import VueRouter from 'vue-router'
+
+// const router = new VueRouter({
+//   routes: [
+//     { name: 'Index', path: '/' },
+//     { name: 'App', path: '/swiper-test' },
+//   ],
+// })
+
 Vue.use(VueGtag, {
   config: {
     id: 'G-HTJ35SW3G3',
